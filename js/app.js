2dbb2df1d55bb918609750ee75a2c4791c22d850
noinvis <lazizbek.sn@gmail.com>
2025-05-09 20:15:00 +0500

    Loading-Animation

---
+++ js/app.js
@@ -0,0 +1,120 @@
+{
+    // 1 - task 
+    // function numTwice(arr){
+        
+        // 1 - variant
+        // return arr.map( i => i * 2 )
+        
+        // 2 - variant
+        // let newArr = []
+        // arr.forEach((i) => {
+        //     newArr.push(i * 2);            
+        // })
+        // return newArr
+
+        // 3 - variant
+        // let yangiarr = []
+        // for (let i = 0; i < arr.length; i++) {
+        //     yangiarr.push(arr[i] * 2)
+        // }
+        // return yangiarr
+    // }
+    // console.log( numTwice([1,2,3]) );
+}
+
+{
+    // 2 - task 
+    // function mrPlus(arr){
+
+        // 1 - variant
+        // return arr.map( i => "Mr." + i );
+        
+        // 2 - variant
+        // let newArr = []
+        // arr.forEach( i => newArr.push("Mr." + i ))
+        // return newArr
+
+        // 3 - variant
+        // let yangiArr = []
+        // for (let i = 0; i < arr.length; i++) yangiArr.push("Mr." + arr[i])
+        // return yangiArr
+    // }
+    // console.log( mrPlus(["John", "Akmal"]) );
+}
+
+{
+    // 3 - task 
+    // function findOdd(arr){
+        
+        // 1 - variant
+        // return arr.filter((i) => i % 2 === 1)
+        
+        // 2 - variant
+        // let newArr = []
+        // arr.forEach((i) => {
+        //     if( i % 2 === 1) newArr.push(i)
+        // })
+        // return newArr
+        
+        // 3 - variant 
+        // let yangiArr = []
+        // for (let i = 0; i < arr.length; i++){
+        //     if( arr[i] % 2 === 1 ) yangiArr.push(arr[i])
+        // }
+        // return yangiArr
+    // }
+    // console.log( findOdd([1,2,3,4]) );
+}
+
+{
+    // 4 - task 
+    // function plusEven(arr){
+        
+        // 1 - variant
+        // return arr.reduce((sum, num) => {
+        //     if (num % 2 === 0) {
+        //         return sum + num;
+        //     }
+        //     return sum;
+        // }, 0);
+        
+        // 2 - variant
+        // let sum = 0
+        // arr.forEach( i => {
+        //     if ( i % 2 === 0) {
+        //         sum += i;
+        //     }
+        // });
+        // return sum;
+        
+        // 3 - variant 
+        // let sum = 0
+        // for (let i = 0; i < arr.length; i++){
+        //     if( arr[i] % 2 === 0 ) sum += arr[i] 
+        // }
+        // return sum
+    // }
+    // console.log( plusEven([1,2,3,4]) );
+}
+
+{
+    // 5 - task 
+    // function countLetter(arr) {
+        
+        // 1 - variant
+        // return arr.map( i => i.length);
+        
+        // 2 - variant
+        // let newArr = []
+        // arr.forEach((i) => {
+        //     newArr.push( i.length );            
+        // })
+        // return newArr
+        
+        // 3 - variant
+        // let yangiArr = []
+        // for (let i = 0; i < arr.length; i++) yangiArr.push( arr[i].length )
+        // return yangiArr
+    // }
+    // console.log( countLetter(["Lorem", "hi"]) );    
+}
